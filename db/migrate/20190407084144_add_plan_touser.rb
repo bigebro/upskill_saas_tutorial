490f2b8b9d799f57d824714250ce10608b8afd9e
Bilguun Enkhee <bigeservice@gmail.com>
2019-04-07 08:55:20 +0000

    Added association for plans and users

---
+++ db/migrate/20190407084144_add_plan_touser.rb
@@ -0,0 +1,4 @@
+class AddPlanTouser < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190406181905) do
+ActiveRecord::Schema.define(version: 20190407084917) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
